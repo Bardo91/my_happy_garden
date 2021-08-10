51cef09e369022dc839277c17904f79a2cdf81be
Bardo91 <19bardo91@gmail.com>
2021-08-10 15:00:52 +0200

    Initialized app

---
+++ lib/main.dart
@@ -0,0 +1,105 @@
+import 'dart:math' as math show pi;
+
+import 'package:collapsible_sidebar/collapsible_sidebar.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My Happy Garden',
+      home: Scaffold(
+        body: SidebarPage(),
+      ),
+    );
+  }
+}
+
+class SidebarPage extends StatefulWidget {
+  @override
+  _SidebarPageState createState() => _SidebarPageState();
+}
+
+class _SidebarPageState extends State<SidebarPage> {
+  List<CollapsibleItem> _items = [];
+  String _headline = "";
+  NetworkImage _avatarImg = NetworkImage('https://www.w3schools.com/howto/img_avatar.png');
+
+  @override
+  void initState() {
+    super.initState();
+    _items = _generateItems;
+    _headline = _items.firstWhere((item) => item.isSelected).text;
+  }
+
+  List<CollapsibleItem> get _generateItems {
+    return [
+      CollapsibleItem(
+        text: 'Calendar',
+        icon: Icons.calendar_view_month,
+        onPressed: () => setState(() => _headline = 'Calendar'),
+        isSelected: true,
+      ),
+      CollapsibleItem(
+        text: 'Wiki',
+        icon: Icons.info,
+        onPressed: () => setState(() => _headline = 'Wiki'),
+      ),
+      CollapsibleItem(
+        text: 'Album',
+        icon: Icons.photo_library,
+        onPressed: () => setState(() => _headline = 'Album'),
+      ),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: CollapsibleSidebar(
+        items: _items,
+        avatarImg: _avatarImg,
+        title: 'John Smith',
+        onTitleTap: () {
+          ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('Yay! Flutter Collapsible Sidebar!')));
+        },
+        body: _body(size, context),
+        backgroundColor: Colors.black,
+        selectedTextColor: Colors.limeAccent,
+        textStyle: TextStyle(fontSize: 15, fontStyle: FontStyle.italic),
+        titleStyle: TextStyle(
+            fontSize: 20,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.bold),
+        toggleTitleStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+
+  Widget _body(Size size, BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      color: Colors.blueGrey[50],
+      child: Center(
+        child: Transform.rotate(
+          angle: math.pi / 2,
+          child: Transform.translate(
+            offset: Offset(-size.height * 0.3, -size.width * 0.23),
+            child: Text(
+              _headline,
+              style: Theme.of(context).textTheme.headline1,
+              overflow: TextOverflow.visible,
+              softWrap: false,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
